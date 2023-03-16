c22f77fdb8367d37c84283837aa2e08c92c7bfe5
QuangHD <quanghd@gearinc.com>
2023-03-16 18:03:40 +0700

    Update Layout component

--- components/layout/index.js
+++ components/layout/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import Image from "next/image";
 import titleImg from "@/assets/images/title.jpg";
 
-const Layout = ({ children }) => (
-  <>
-    <div>
-      <Image src={titleImg} alt="Ghi điểm tiến lên" className="w-28 m-auto" />
+const Layout = ({ children, footer }) => (
+  <div className="flex flex-col">
+    <div className="w-28">
+      <Image src={titleImg} alt="Ghi điểm tiến lên" className="w-full m-auto" />
     </div>
-    <div className="px-4">{children}</div>
-  </>
+    <div className="px-4 flex-1">{children}</div>
+    {footer && <div>{footer}</div>}
+  </div>
 );
 
 export default Layout;
---
+++ pages/game/[id]/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "@/components/layout";
+
+const GameDetail = () => (
+  <Layout>
+    <div>GameDetail</div>
+  </Layout>
+);
+
+export default GameDetail;
