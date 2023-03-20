4d4af844592c5dc162def6354988dabb9d85abb4
QuangHD <quanghd@gearinc.com>
2023-03-20 16:31:16 +0700

    Update favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,22 @@
 import "@/styles/globals.css";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import Head from "next/head";
 import { store, persistor } from "../redux/store";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <div className="min-h-[896px]">
-          <Component {...pageProps} />
-        </div>
-      </PersistGate>
-    </Provider>
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.png" />
+      </Head>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <div className="min-h-[896px]">
+            <Component {...pageProps} />
+          </div>
+        </PersistGate>
+      </Provider>
+    </>
   );
 }
